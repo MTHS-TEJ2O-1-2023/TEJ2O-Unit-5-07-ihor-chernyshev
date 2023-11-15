3e99d0a744b95717566d014812c365239cf9e144
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-15 23:41:58 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -18,7 +18,7 @@ _STP_CHD_H = 1023
 
 
 class PCA9685:
-    def __init__(self):
+    def init(self):
         self.write(_PCA9685_ADDRESS, _MODE1, 0x00)
         self.setFreq(50)
         for i in range(0, 16):
@@ -65,6 +65,46 @@ class PCA9685:
     def releaseServo(self, servo):  # servo: 1, etc.
         self.setPwm(servo + 7, 0, 0)
 
+    def setStepper(self, index, dir):  # index: 1 or 2, dir: True or False
+        if index == 1:
+            if dir:
+                self.setPwm(0, _STP_CHA_L, _STP_CHA_H)
+                self.setPwm(2, _STP_CHB_L, _STP_CHB_H)
+                self.setPwm(1, _STP_CHC_L, _STP_CHC_H)
+                self.setPwm(3, _STP_CHD_L, _STP_CHD_H)
+            else:
+                self.setPwm(3, _STP_CHA_L, _STP_CHA_H)
+                self.setPwm(1, _STP_CHB_L, _STP_CHB_H)
+                self.setPwm(2, _STP_CHC_L, _STP_CHC_H)
+                self.setPwm(0, _STP_CHD_L, _STP_CHD_H)
+        else:
+            if dir:
+                self.setPwm(4, _STP_CHA_L, _STP_CHA_H)
+                self.setPwm(6, _STP_CHB_L, _STP_CHB_H)
+                self.setPwm(5, _STP_CHC_L, _STP_CHC_H)
+                self.setPwm(7, _STP_CHD_L, _STP_CHD_H)
+            else:
+                self.setPwm(7, _STP_CHA_L, _STP_CHA_H)
+                self.setPwm(5, _STP_CHB_L, _STP_CHB_H)
+                self.setPwm(6, _STP_CHC_L, _STP_CHC_H)
+                self.setPwm(4, _STP_CHD_L, _STP_CHD_H)
+
+    def stopStepper(self, index):
+        for i in range(0, 4) if (index == 1) else range(4, 8):
+            self.setPwm(i, 0, 0)
+
+    def moveStepperDegrees(self, index, degrees):  # index: 1 or 2
+        self.setStepper(index, degrees > 0)
+        delta_ms = round(abs(10240 * abs(degrees) / 360))
+        return delta_ms  # returns milliseconds to stop
+
+    def moveStepperDegreesBlocking(self, index, degrees):  # index: 1 or 2
+        delta_ms = self.moveStepperDegrees(index, degrees)
+        time.sleep_ms(delta_ms)
+        self.stopStepper(index)
+
+    def startStepper(self, index, clockwise=True):  # index: 1 or 2
+        self.setStepper(index, clockwise)
 
 pca = PCA9685()
 
